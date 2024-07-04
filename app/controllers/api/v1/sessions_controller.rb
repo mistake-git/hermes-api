1085afe38b18a9311f9261f817ec4343854d4a26
mistake-git <acbmstk0402@gmail.com>
2024-07-04 21:47:25 +0900

    タイムアウト検証

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::SessionsController < AuthController
   def create
     user = User.find_or_create_by(user_params)
+    render json: { status: :ok }
     if user
       token = encode_token(user_id: user.id)
       render json: { token: token }, status: :ok
