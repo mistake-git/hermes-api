16fe87257725daaa46b48cfd62cfe54475d44deb
mistake-git <acbmstk0402@gmail.com>
2024-07-08 17:33:40 +0900

    並び順修正

--- app/controllers/api/v1/homes_controller.rb
+++ app/controllers/api/v1/homes_controller.rb
@@ -1,11 +1,11 @@
 class Api::V1::HomesController < ApplicationController
   def interviews
-    interviews = current_user.interviews.where('interview_time_from <= ?', 1.week.from_now)
-  render json: interviews, each_serializer: InterviewSerializer
+    interviews = current_user.interviews.where('interview_time_from <= ?', 1.week.from_now).order(interview_time_from: :desc)
+    render json: interviews, each_serializer: InterviewSerializer
   end
 
   def job_application_todos
-    job_application_todos = current_user.job_application_todos.where('deadline <= ?', 1.week.from_now)
+    job_application_todos = current_user.job_application_todos.where('deadline <= ?', 1.week.from_now).order(deadline: :desc)
     render json: job_application_todos
   end
 end
